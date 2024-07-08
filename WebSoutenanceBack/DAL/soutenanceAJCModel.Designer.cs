7604c1a418c733db65bbde91b38aa12c3f0c86bd
morgan <morgan.dagron@gmail.com>
2024-07-08 12:50:37 +0200

    RIen ?

--- WebSoutenanceBack/DAL/soutenanceAJCModel.Designer.cs
+++ WebSoutenanceBack/DAL/soutenanceAJCModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La génération de code T4 est activée pour le modèle 'C:\Users\devil\Documents\GitHub\soutenanceAJCBack\WebSoutenanceBack\DAL\soutenanceAJCModel.edmx'. 
+﻿// La génération de code T4 est activée pour le modèle 'S:\Travail\soutenanceAJC\WebSoutenanceBack\WebSoutenanceBack\DAL\soutenanceAJCModel.edmx'. 
 // Pour activer la génération de code héritée, définissez la valeur de la propriété
 // du concepteur 'Stratégie de génération de code' sur 'ObjectContext hérité'. Cette propriété est disponible dans la fenêtre Propriétés lorsque le modèle
 //  est ouvert dans le concepteur.
