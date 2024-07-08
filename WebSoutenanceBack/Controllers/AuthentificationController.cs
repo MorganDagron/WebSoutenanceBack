95c3c811c84430e0fcbd54859e7af90c9aee9b7f
LebrunLaurentDylan <131178706+LebrunLaurentDylan@users.noreply.github.com>
2024-07-08 10:27:01 +0200

    Update AuthentificationController.cs

--- WebSoutenanceBack/Controllers/AuthentificationController.cs
+++ WebSoutenanceBack/Controllers/AuthentificationController.cs
@@ -5,7 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using WebSoutenanceBack.DAL;
-
+// comment 
 namespace WebSoutenanceBack.Controllers
 {
     public class AuthentificationController : ApiController
